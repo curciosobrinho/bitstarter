7b5f9c45a8b22b29d4b8f4eff9b2467b56d3353f
curcio Sobrinho <curcio@me.com>
2013-07-05 18:50:58 +0000

    Updated with index web.js

--- web.js
+++ web.js
@@ -3,14 +3,18 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var html;
+
+fs.readFile('index.html', function (err, data) {                                                                
+  if (err) throw err;                                                                                             
+  html = new Buffer(data, "utf-8");                                                                           
+                                                                          
+                                                                                                                  
+});  
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  var html = new Buffer(data, "utf-8");
-  html.toString('utf-8');
+    response.send(html.toString('utf-8'));
 
-});
 
 });
 
@@ -18,3 +22,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
